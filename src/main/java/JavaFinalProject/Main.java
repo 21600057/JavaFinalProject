f2a8831826668c49bf06eca66269f3fef029213a
동준 <@DESKTOP-FS8K790>
2019-06-17 02:24:40 +0900

    commit

---
+++ src/main/java/JavaFinalProject/Main.java
@@ -0,0 +1,28 @@
+package JavaFinalProject;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+import java.io.File;
+
+public class Main 
+{
+	String path1, path2; // 읽을 파일 경로와 써질 파일 경로 두개를 받음.
+	
+	public static void main(String[] args) 
+	{ 
+
+		Main finalStart = new Main();
+		finalStart.run(args);
+	}
+	
+	public void run(args)
+	{
+		
+	}
+	
+}
+ 
