c11ee70a05a47d8896c4c6aa522da9e9859b5455
동준 <@DESKTOP-FS8K790>
2019-06-17 02:47:49 +0900

    commit

--- src/main/java/JavaFinalProject/Main.java
+++ src/main/java/JavaFinalProject/Main.java
@@ -6,23 +6,24 @@ import org.apache.commons.cli.DefaultParser;
 import org.apache.commons.cli.HelpFormatter;
 import org.apache.commons.cli.Option;
 import org.apache.commons.cli.Options;
-import java.io.File;
+
 
 public class Main 
 {
-	String path1, path2; // 읽을 파일 경로와 써질 파일 경로 두개를 받음.
+	String path1, path2; // 읽을 파일 경로와 써질 파일.
 	
 	public static void main(String[] args) 
 	{ 
 
-		Main finalMain = new Main();
-		finalMain.run(args);
+		Main main = new Main();
+		main.run(args);
 	}
 	
 	public void run(String[] args)
 	{
 		path1 = args[0];
 		path2 = args[1];
+		System.out.println("path1 = " + path1);
 	}
 	
 }
