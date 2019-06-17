218513f3454958e60f4a7496e1d19d3f2177c1a3
동준 <@DESKTOP-FS8K790>
2019-06-17 15:57:23 +0900

    commit

--- src/main/java/JavaFinalProject/ExcelReader.java
+++ src/main/java/JavaFinalProject/ExcelReader.java
@@ -13,15 +13,13 @@ import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.ss.usermodel.WorkbookFactory;
 
 public class ExcelReader 
-{
-	
-	public ArrayList<String> getData(String path) 
+{	
+	public ArrayList<String> getData(InputStream is) 
 	{
 		ArrayList<String> values = new ArrayList<String>();
 		
 		try (InputStream inp = is) 
 		{
-		    InputStream inp = new FileInputStream("workbook.xlsx");
 		    
 		        Workbook wb = WorkbookFactory.create(inp);
 		        
@@ -36,7 +34,19 @@ public class ExcelReader
 		        	for (int j=0; j<row.getPhysicalNumberOfCells(); j++)
 		        	{
 		        		cell = row.getCell(j);
-				        values.add(cell.getStringCellValue());
+		        		switch(cell.getCellType())
+		        		{
+		        			case NUMERIC:
+		        			 values.add(Double.toString((int)cell.getNumericCellValue()));
+		        			 break;
+		        		
+		        			case STRING:
+		        			 values.add(cell.getStringCellValue());
+		        			 break;
+		        		
+		        			default:
+		        			 break;
+		        		}
 		        	}
 		        }
 		        
@@ -53,43 +63,4 @@ public class ExcelReader
 		
 		return values;
 	}
-	/*
-	public ArrayList<String> getData(InputStream is) 
-	{
-		ArrayList<String> values = new ArrayList<String>();
-		
-		try (InputStream inp = is) 
-		{
-		    //InputStream inp = new FileInputStream("workbook.xlsx");
-		    
-		        Workbook wb = WorkbookFactory.create(inp);
-		        
-		        Sheet sheet = wb.getSheetAt(0);
-		        Row row;
-		        Cell cell;
-		        int numRow = sheet.getPhysicalNumberOfRows();
-		        
-		        for (int i=0; i<numRow; i++)
-		        {
-		        	row = sheet.getRow(i);
-		        	for (int j=0; j<row.getPhysicalNumberOfCells(); j++)
-		        	{
-		        		cell = row.getCell(j);
-				        values.add(cell.getStringCellValue());
-		        	}
-		        }
-		        
-		        
-		    } catch (FileNotFoundException e) 
-		{
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-		} catch (IOException e) 
-		{
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-		}
-		
-		return values;
-	}*/
 }
